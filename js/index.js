276dd7a79a10337ddd385111487687d4f5b571bb
mlampert84 <marcus.lampert@gmail.com>
2018-03-22 21:29:31 +0100

    Add project description of demo voting app

--- js/index.js
+++ js/index.js
@@ -6,19 +6,41 @@ $(document).ready(function() {
 
   matchFlipcardFaceDimensions();
 
-  $('.toggleElement').click(function() {
+  $('.toggleFront').click(function() {
+    var heightBack = $(this).closest('.flipcard')
+             .find('.toggleBack').height();
+    var heightFront = $(this).height();
+
+    $(this).animate({marginTop:(heightBack - heightFront)},2000);
+
     $(this)
       .closest('.flipcard')
-      .toggleClass('flipped');
+      .addClass('flipped');
   });
+
+  $('.toggleBack').click(function() {
+    $(this).closest('.flipcard')
+           .find('.toggleFront')
+           .animate({marginTop:0},2000);
+
+    $(this)
+      .closest('.flipcard')
+      .removeClass('flipped');
+  });
+
+  $( ".btn" ).click(function( event ) {
+  event.stopPropagation();
+});
+
 });
 
 $(window).resize(function() {
   matchFlipcardFaceDimensions();
+  fitToggleBackHeightOnResize();
 });
 
+//this function matches the size of the backface of the flipcard to the image on the front of the card.
 function matchFlipcardFaceDimensions() {
-  //this function matches the size of the backface of the flipcard to the image on the front of the card.
   $('.face.back').each(function() {
     var height = $(this)
       .closest('.flipcard')
@@ -28,7 +50,20 @@ function matchFlipcardFaceDimensions() {
       .closest('.flipcard')
       .find('img')
       .width();
-    $(this).height(height);
+    $(this).css('min-height', height + 'px');
     $(this).width(width);
   });
+}
+
+//this function is designed to change the margin height of the front element as needed
+//to give the back element its space.
+function fitToggleBackHeightOnResize(){
+  $('.flipped').each(function(){
+    var toggleFront = $(this).find('.toggleFront');
+    var heightBack = $(this).find('.toggleBack').height();
+    var heightFront = toggleFront.height();
+
+    toggleFront.css('margin-top',heightBack-heightFront+'px');
+}
+  )
 }
